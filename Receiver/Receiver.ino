ef34a96b930a61650523257f2619782c1d9ac015
ddella <48737096+ddella@users.noreply.github.com>
2019-03-23 17:23:57 -0400

    Update Receiver.ino

--- Receiver/Receiver.ino
+++ Receiver/Receiver.ino
@@ -124,61 +124,18 @@
 
 /*Include the VirtualWire library */
 #include <VirtualWire.h>
-
-/* Just comment the "KEEPALIVE" if you don't want to test the keepalives
- * sent from the remote at regular interval.
- */
-#define KEEPALIVE
-
-/*
- * Digital IO pin from the RF receiver. Can be any digital pin.
- */
-#define RX_DIO_PIN 4
-#define MAX_MESSAGE_LEN 7 //Joystick sends a max of 7 bytes
-#define TX_SPEED 2000 //Transmission speed in bits/sec
-
-/*
- * Preamble byte in each different frames sent by the remote
- */
-#define STATUS_PREAMBULE    0xAA
-#define KEEPALIVE_PREAMBULE 0xE0
-
-#define INTERVAL_500ms       500UL
-#define KEEPALIVE_INTERVAL   3000UL //Keepalive sent every 3 sec
-
-/*
- * Keys byte
- *   K1 pressed = 0b00000001 (0x01)
- *   K2 pressed = 0b00000010 (0x02)
- *   K3 pressed = 0b00000100 (0x04)
- *   K4 pressed = 0b00001000 (0x08)
- *   KZ pressed = 0b00010000 (0x10) (Joystick's button)
- */
-#define JOYSTICK_BUTTON_K1_PRESSED 0b00000001 // (0x01)
-#define JOYSTICK_BUTTON_K2_PRESSED 0b00000010 // (0x02)
-#define JOYSTICK_BUTTON_K3_PRESSED 0b00000100 // (0x04)
-#define JOYSTICK_BUTTON_K4_PRESSED 0b00001000 // (0x08)
-#define JOYSTICK_BUTTON_KZ_PRESSED 0b00010000 // (0x10)
-#define JOYSTICK_NO_BUTTON_PRESSED 0b00000000 // (0x00) no button pressed
-
-#define WORD(MSB,LSB) (((uint16_t)MSB << 8) | LSB)  //Returns a uint16 from two uint8
-
-/* Global variables for schedulers */
-#ifdef KEEPALIVE
-uint32_t previousMillis_500ms;
-uint32_t LastKeepalive; //millis() timestamp of the last keepalive received
-#endif
+#include "Receiver.h"
 
 /* Global variable for last button's status */
-uint8_t LastButtonPressed;
+uint8_t LastButtonPressed = JOYSTICK_NO_BUTTON_PRESSED;
 
 /* Global variable for current & last x-axis position */
-uint16_t Last_X_Axis;
-uint16_t Current_X_Axis;
+uint16_t Last_X_Axis = 505;
+uint16_t Current_X_Axis = 505;
 
 /* Global variable for current & last y-axis position */
-uint16_t Last_Y_Axis;
-uint16_t Current_Y_Axis;
+uint16_t Last_Y_Axis = 505;
+uint16_t Current_Y_Axis = 505;
 
 void setup() {
   Serial.begin(9600);
@@ -211,100 +168,118 @@ void loop() {
   uint8_t RxBuffer[MAX_MESSAGE_LEN];     //Buffer that contains the received data
   uint8_t FCS; //Calculated checksum from the received packet
 
-    // If a packet is received, we store it with it's length
-    if (vw_get_message(RxBuffer, &Buffer_Size)) {
-     //Remote sends fixed 7-byte packets for either status and keepalive
-      if (Buffer_Size == MAX_MESSAGE_LEN) {
-        FCS = Checksum (RxBuffer, MAX_MESSAGE_LEN - 1);//Caculate checksum from the buffer received
-        if (FCS == RxBuffer [MAX_MESSAGE_LEN - 1]) {
-          //RxBuffer [0] is the preamble. Either status or keepalive
-          switch (RxBuffer [0]) { // Check if we received a status or a keepalive
-            case STATUS_PREAMBULE:
-              Current_X_Axis = WORD(RxBuffer [2], RxBuffer [3]);
-              Current_Y_Axis = WORD(RxBuffer [4], RxBuffer [5]);
-              if (Current_X_Axis != Last_X_Axis) {
-                Serial.print (F("X axis value changed = ")); Serial.print (Current_X_Axis);
-                Serial.print (F(" :"));
-                Last_X_Axis = Current_X_Axis;
-              }
-              if (Current_Y_Axis != Last_Y_Axis) {
-                Serial.print (F("Y axis value changed = ")); Serial.print (Current_Y_Axis);
-                Last_Y_Axis = Current_Y_Axis;
-                Serial.print (F(" :"));
-              }
-
-               //RxBuffer [1] is the button byte. (which button has been pressed)
-               switch (RxBuffer [1]) {
-                 case JOYSTICK_BUTTON_K1_PRESSED:
-                   Serial.print (F("Button K1 pressed: "));
-                   LastButtonPressed = JOYSTICK_BUTTON_K1_PRESSED;
-                 break;
-                 case JOYSTICK_BUTTON_K2_PRESSED:
-                   Serial.print (F("Button K2 pressed: "));
-                   LastButtonPressed = JOYSTICK_BUTTON_K2_PRESSED;
-                 break;
-                 case JOYSTICK_BUTTON_K3_PRESSED:
-                   Serial.print (F("Button K3 pressed: "));
-                   LastButtonPressed = JOYSTICK_BUTTON_K3_PRESSED;
-                 break;
-                 case JOYSTICK_BUTTON_K4_PRESSED:
-                   Serial.print (F("Button K4 pressed: "));
-                    LastButtonPressed = JOYSTICK_BUTTON_K4_PRESSED;
-                 break;
-                 case JOYSTICK_BUTTON_KZ_PRESSED:
-                   Serial.print (F("Button KZ pressed: "));
-                   LastButtonPressed = JOYSTICK_BUTTON_KZ_PRESSED;
-                 break;
-                 case JOYSTICK_NO_BUTTON_PRESSED:
-                   if (LastButtonPressed == JOYSTICK_BUTTON_K1_PRESSED)
-                     Serial.print (F("Button K1 released: "));
-                   else if (LastButtonPressed == JOYSTICK_BUTTON_K2_PRESSED)
-                     Serial.print (F("Button K2 released: "));
-                   else if (LastButtonPressed == JOYSTICK_BUTTON_K3_PRESSED)
-                     Serial.print (F("Button K3 released: "));
-                   else if (LastButtonPressed == JOYSTICK_BUTTON_K4_PRESSED)
-                     Serial.print (F("Button K4 released: "));
-                   else if (LastButtonPressed == JOYSTICK_BUTTON_KZ_PRESSED)
-                     Serial.print (F("Button KZ released: "));
-                   LastButtonPressed = RxBuffer [1];  
-                 break;
-               }//switch (RxBuffer [1])
-             break;
-            #ifdef KEEPALIVE
-            case KEEPALIVE_PREAMBULE:
-              Serial.print ("Keepalive: ");
-              LastKeepalive = millis();//Reset keepalive timer
-            break;
-            #endif
-          }//switch (RxBuffer [0])
-          PrintBuf (RxBuffer, MAX_MESSAGE_LEN);
-        }//if FCS
-      }//if length
-    }//ifvw_get_message
-
-  /* 
-   *  This block is executed every 500 ms to check if keepalive are
-   *  received from remote at the correct interval.
+  while (1) {
+  /*
+   * Check to see if we received a packet
    */
-#ifdef KEEPALIVE
-  if (millis() - previousMillis_500ms >= INTERVAL_500ms) {
-    previousMillis_500ms = millis();
-/*
- * This code is "overflow" safe. Even if millis() overflow after 49 days, the test
- * will be valid.
- * 
- * !!! The following will NOT work !!!
- * if (millis()  > LastKeepalive + KEEPALIVE_INTERVAL)
- * 
- */
-    if (millis() - LastKeepalive > KEEPALIVE_INTERVAL) {
-      //Keepalive not received!!!
-      Serial.print(F("Keepalive not received. Last keepalive received: "));
-      Serial.println (millis() - LastKeepalive);
-    }
-  }
-#endif
+    if (vw_get_message(RxBuffer, &Buffer_Size)) {
+    //if packet is not the correct length, then just skip. Not sent from our remote.
+      if (Buffer_Size != MAX_MESSAGE_LEN)
+        continue;
+      //Caculate checksum of the received packet.
+      FCS = Checksum (RxBuffer, MAX_MESSAGE_LEN - 1);
+      //if checksum is wrong, then just skip. Not sent from our remote or bad packet.
+      if (FCS != RxBuffer [MAX_MESSAGE_LEN - 1])
+        continue;
+
+      /*
+       * We received a packet of the correct length and with a valid checksum.
+       * We need to verify the type of packet by looking at the preambule byte.
+       * RxBuffer [0] is the preamble. It can be either status or keepalive
+       */
+      switch (RxBuffer [0]) {
+        case STATUS_PREAMBULE:
+          Preamble (RxBuffer);
+          PrintBuf (RxBuffer, MAX_MESSAGE_LEN);
+        break;
+        #ifdef KEEPALIVE
+        case KEEPALIVE_PREAMBULE:
+          Serial.print ("Keepalive: ");
+          PrintBuf (RxBuffer, MAX_MESSAGE_LEN);
+          LastKeepalive = millis();//Reset keepalive timer
+        break;
+        #endif
+      }//switch (RxBuffer [0])
+    } // if packet received
+
+    /* 
+     *  This block is executed every 500 ms to check if keepalive are
+     *  received from remote at the correct interval.
+     */
+    #ifdef KEEPALIVE
+    if (millis() - previousMillis_500ms >= INTERVAL_500ms) {
+      previousMillis_500ms = millis();
+    /*
+     * This code is "overflow" safe. Even if millis() overflow after 49 days, the test
+     * will be valid.
+     * 
+     * !!! The following will NOT work !!!
+     * if (millis()  > LastKeepalive + KEEPALIVE_INTERVAL)
+     * 
+     */
+      if (millis() - LastKeepalive > KEEPALIVE_INTERVAL) {
+        //Keepalive not received!!!
+        Serial.print(F("Keepalive not received. Last keepalive received: "));
+        Serial.println (millis() - LastKeepalive);
+      }
+    } //if >= 500 msec
+    #endif
+  } //while (1)
+
 }//loop()
+
+void Preamble (const uint8_t *RxBuffer) {
+
+  Current_X_Axis = WORD(RxBuffer [2], RxBuffer [3]);
+  Current_Y_Axis = WORD(RxBuffer [4], RxBuffer [5]);
+  if (Current_X_Axis != Last_X_Axis) {
+    Serial.print (F("X axis value changed = ")); Serial.print (Current_X_Axis);
+    Serial.print (F(" :"));
+    Last_X_Axis = Current_X_Axis;
+  }
+  if (Current_Y_Axis != Last_Y_Axis) {
+    Serial.print (F("Y axis value changed = ")); Serial.print (Current_Y_Axis);
+    Last_Y_Axis = Current_Y_Axis;
+    Serial.print (F(" :"));
+  }
+
+   //RxBuffer [1] is the button byte. (which button has been pressed)
+   switch (RxBuffer [1]) {
+     case JOYSTICK_BUTTON_K1_PRESSED:
+       Serial.print (F("Button K1 pressed: "));
+       LastButtonPressed = JOYSTICK_BUTTON_K1_PRESSED;
+     break;
+     case JOYSTICK_BUTTON_K2_PRESSED:
+       Serial.print (F("Button K2 pressed: "));
+       LastButtonPressed = JOYSTICK_BUTTON_K2_PRESSED;
+     break;
+     case JOYSTICK_BUTTON_K3_PRESSED:
+       Serial.print (F("Button K3 pressed: "));
+       LastButtonPressed = JOYSTICK_BUTTON_K3_PRESSED;
+     break;
+     case JOYSTICK_BUTTON_K4_PRESSED:
+       Serial.print (F("Button K4 pressed: "));
+        LastButtonPressed = JOYSTICK_BUTTON_K4_PRESSED;
+     break;
+     case JOYSTICK_BUTTON_KZ_PRESSED:
+       Serial.print (F("Button KZ pressed: "));
+       LastButtonPressed = JOYSTICK_BUTTON_KZ_PRESSED;
+     break;
+     case JOYSTICK_NO_BUTTON_PRESSED:
+       if (LastButtonPressed == JOYSTICK_BUTTON_K1_PRESSED)
+         Serial.print (F("Button K1 released: "));
+       else if (LastButtonPressed == JOYSTICK_BUTTON_K2_PRESSED)
+         Serial.print (F("Button K2 released: "));
+       else if (LastButtonPressed == JOYSTICK_BUTTON_K3_PRESSED)
+         Serial.print (F("Button K3 released: "));
+       else if (LastButtonPressed == JOYSTICK_BUTTON_K4_PRESSED)
+         Serial.print (F("Button K4 released: "));
+       else if (LastButtonPressed == JOYSTICK_BUTTON_KZ_PRESSED)
+         Serial.print (F("Button KZ released: "));
+       LastButtonPressed = RxBuffer [1];  
+     break;
+   }//switch (RxBuffer [1])
+} //Preamble (const uint8_t *RxBuffer)
 
 /*
  * Simple checksum function. Returns the checksum of an array of bytes.
