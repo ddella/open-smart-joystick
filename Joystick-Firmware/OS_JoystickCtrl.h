60b490098333dea6b522a55710a2426325b05712
ddella <48737096+ddella@users.noreply.github.com>
2019-03-26 18:12:51 -0400

    Update OS_JoystickCtrl.h

--- Joystick-Firmware/OS_JoystickCtrl.h
+++ Joystick-Firmware/OS_JoystickCtrl.h
@@ -3,8 +3,7 @@
 
 #include <Arduino.h>
 
-#define DEBOUNCE_DELAY_KEYS     10 // in msec
-#define DEBOUNCE_DELAY_JOYSTICK 10 // in msec
+#define DEBOUNCE_DELAY_BUTTON   4000 // in microsec
 
 #define DIO_KEY1 5 //Button K1
 #define DIO_KEY2 4 //Button K2
@@ -26,39 +25,42 @@ enum keys_idx {k1, k2, k3, k4, kz};
 #define X_SMOOTHING 8 //Minimum movement of X axis before we consider a change
 #define Y_SMOOTHING 8 //Minimum movement of Y axis before we consider a change
 
-#define ANALOG_READING 2       // Number of analogRead (samples) for the joystick axis
-#define DELAY_ANALOG_READING 50 // Delay between each analogRead, in microsec
+/*
+* Preamble byte for each type of packets sent by the remote
+*/
+
+#define STATUS_PREAMBULE    0xAA
+#define KEEPALIVE_PREAMBULE 0xE0
 
 class JoystickCtrl {
 private:
 /*
- * _Keys byte
+ * _Buttons byte
  * K1 pressed = 0b00000001 (0x01)
  * K2 pressed = 0b00000010 (0x02)
  * K3 pressed = 0b00000100 (0x04)
  * K4 pressed = 0b00001000 (0x08)
  * KZ pressed = 0b00010000 (0x10) (Joystick button)
  */
-  uint8_t  _Keys;
-  uint8_t _Old_Keys;
+  uint8_t  _Buttons;
+  uint8_t  _Old_Buttons;
   uint16_t _X_Axis, _Y_Axis; //Analog value for joystick pins
-  uint16_t _X_Axis_Zero, _Y_Axis_Zero; //Initial value of each axis, around 1023/2
-  uint16_t _X_AxisOld, _Y_AxisOld; //Values for joystick pins
-  uint8_t _Key_Pin [MAX_KEY]; //Array of I/O pins for all the button
+  uint16_t _X_AxisOld, _Y_AxisOld; //Last values for joystick pins
+  uint8_t  _Key_Pin [MAX_KEY]; //Array of I/O pins for all the buttons
 
-  uint8_t  readKeys ();   //Digital read of all the buttons of the remote                     
-  uint16_t read_Axis (uint8_t);  //Analog read of a joystick's axis
+  uint8_t  _readButtons ();   //Digital read of all the buttons of the remote                     
+  uint16_t _read_Axis (uint8_t);  //Analog read of a joystick's x or y axis
 
 public:
   JoystickCtrl ();
   void init();
-  uint8_t  getKeys ();  //Returns the encoded keys pressed in a single byte.
+  uint8_t  getButtons (); //Returns the encoded button, pressed/released, in a single byte.
 
   uint16_t getX_Axis (); //Returns variable _X_Axis as uint16_t: Public
   uint16_t getY_Axis (); //Returns variable _Y_Axis as uint16_t: Public
 
-  uint8_t asChange (); //Read all sensors on remote and return true if something chnaged.
-  uint8_t Checksum (const uint8_t *data, uint8_t len); //Returns checksum of an array of bytes.
+  bool     asChange (); //Read all sensors on remote and return true if something changed.
+  uint8_t  Checksum (const uint8_t *data, uint8_t len); //Returns checksum of an array of bytes.
 };
 
 #endif
